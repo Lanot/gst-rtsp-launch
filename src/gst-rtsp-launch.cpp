6899a288e00bcecedcd555b18e261849c91d8131
Anatolii Lehkyi <anatolii.lehkyi@codefluent.com>
2025-06-26 13:01:25 +0300

    copied GST folder from original repo, GST tag/version 1.18.4

--- src/gst-rtsp-launch.cpp
+++ src/gst-rtsp-launch.cpp
@@ -1,92 +1,83 @@
-/* GStreamer
- * Copyright (C) 2008 Wim Taymans <wim.taymans at gmail.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- */
-
+#include <iostream>
+#include <string>
 #include <gst/gst.h>
-
 #include <gst/rtsp-server/rtsp-server.h>
 
 #define DEFAULT_RTSP_PORT "8554"
-#define DEFAULT_RTSP_ENDPOINT "/video"
+#define DEFAULT_ENDPOINT "video"
 
-static char *port = (char *) DEFAULT_RTSP_PORT;
-static char *endpoint = (char *) DEFAULT_RTSP_ENDPOINT;
+int main(int argc, char** argv) {
+    GMainLoop *loop;
+    GstRTSPServer *server;
+    GstRTSPMountPoints *mounts;
+    GstRTSPMediaFactory *factory;
+    GOptionContext *optctx;
+    GError *error = NULL;
 
-static GOptionEntry entries[] = {
-  {"port", 'p', 0, G_OPTION_ARG_STRING, &port,
-      "Port to listen on (default: " DEFAULT_RTSP_PORT ")", "PORT"},
-  {"endpoint", 'e', 0, G_OPTION_ARG_STRING, &endpoint,
-    "Endpoint name (default: " DEFAULT_RTSP_ENDPOINT ")", "ENDPOINT"},
-  {NULL}
-};
+    struct {
+        char *port = nullptr;
+        char *endpoint = nullptr;
+    } opts;
 
-int
-main (int argc, char *argv[])
-{
-  GMainLoop *loop;
-  GstRTSPServer *server;
-  GstRTSPMountPoints *mounts;
-  GstRTSPMediaFactory *factory;
-  GOptionContext *optctx;
-  GError *error = NULL;
+    GOptionEntry entries[] = {
+        {"port", 'p', 0, G_OPTION_ARG_STRING, &opts.port, "Port to listen on (default: " DEFAULT_RTSP_PORT ")", "PORT"},
+        {"endpoint", 'e', 0, G_OPTION_ARG_STRING, &opts.endpoint, "Endpoint name (default: " DEFAULT_ENDPOINT ")", "ENDPOINT"},
+        {nullptr}
+    };
 
-  optctx = g_option_context_new ("<launch line> - Test RTSP Server, Launch\n\n"
-      "Example: \"( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )\"");
-  g_option_context_add_main_entries (optctx, entries, NULL);
-  g_option_context_add_group (optctx, gst_init_get_option_group ());
-  if (!g_option_context_parse (optctx, &argc, &argv, &error)) {
-    g_printerr ("Error parsing options: %s\n", error->message);
-    g_option_context_free (optctx);
-    g_clear_error (&error);
-    return -1;
-  }
-  g_option_context_free (optctx);
+    optctx = g_option_context_new("<launch line> - Simple RTSP server\n\n"
+        "Sample pipeline: \"( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )\"");
+    g_option_context_add_main_entries(optctx, entries, NULL);
+    g_option_context_add_group(optctx, gst_init_get_option_group());
+    if (!g_option_context_parse(optctx, &argc, &argv, &error)) {
+        g_printerr("Error parsing options: %s", error->message);
+        g_option_context_free(optctx);
+        g_clear_error(&error);
+        return -1;
+    }
 
-  loop = g_main_loop_new (NULL, FALSE);
+    if (nullptr == argv[1]) {
+        g_printerr("Error: empty pipeline\n");
+        return -1;
+    }
 
-  /* create a server instance */
-  server = gst_rtsp_server_new ();
-  g_object_set (server, "service", port, NULL);
+    g_option_context_free(optctx);
 
-  /* get the mount points for this server, every server has a default object
-   * that be used to map uri mount points to media factories */
-  mounts = gst_rtsp_server_get_mount_points (server);
+    if (nullptr == opts.port) {
+        g_print("Using default port: %s\n", DEFAULT_RTSP_PORT);
+        opts.port = strdup(DEFAULT_RTSP_PORT);
+    } else {
+        g_print("Using port: %s\n", opts.port);
+    }
 
-  /* make a media factory for a test stream. The default media factory can use
-   * gst-launch syntax to create pipelines.
-   * any launch line works as long as it contains elements named pay%d. Each
-   * element with pay%d names will be a stream */
-  factory = gst_rtsp_media_factory_new ();
-  gst_rtsp_media_factory_set_launch (factory, argv[1]);
-  gst_rtsp_media_factory_set_shared (factory, TRUE);
+    if (nullptr == opts.endpoint) {
+        g_print("Using default endpoint: %s\n", DEFAULT_ENDPOINT);
+        opts.endpoint = strdup(DEFAULT_ENDPOINT);
+    } else {
+        g_print("Using endpoint: %s\n", opts.endpoint);
+    }
 
-  /* attach the test factory to the /video url */
-  gst_rtsp_mount_points_add_factory (mounts, endpoint, factory);
+    loop = g_main_loop_new(NULL, FALSE);
 
-  /* don't need the ref to the mapper anymore */
-  g_object_unref (mounts);
+    server = gst_rtsp_server_new();
+    g_object_set(server, "service", opts.port, nullptr);
+    mounts = gst_rtsp_server_get_mount_points(server);
+    factory = gst_rtsp_media_factory_new();
 
-  /* attach the server to the default maincontext */
-  gst_rtsp_server_attach (server, NULL);
+    gst_rtsp_media_factory_set_launch(factory, argv[1]);
+    gst_rtsp_media_factory_set_shared(factory, TRUE);
 
-  /* start serving */
-  g_print ("stream ready at rtsp://127.0.0.1:%s%endpoint\n", port, endpoint);
-  g_main_loop_run (loop);
+    g_print("Using pipeline (as parsed): %s\n", gst_rtsp_media_factory_get_launch(factory));
+    std::string mount = "/";
+    mount += opts.endpoint;
+    gst_rtsp_mount_points_add_factory(mounts, mount.c_str(), factory);
 
-  return 0;
+    g_object_unref(mounts);
+
+    gst_rtsp_server_attach(server, NULL);
+
+    g_print("Stream ready at rtsp://127.0.0.1:%s/%s\n", opts.port, opts.endpoint);
+    g_main_loop_run(loop);
+
+    return 0;
 }
